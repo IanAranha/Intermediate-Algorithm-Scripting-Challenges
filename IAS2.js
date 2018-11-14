2188da0048ce3f781caee3eb9f90b3d4aa467273
Ian Aranha <ianorama@gmail.com>
2018-11-14 14:23:40 -0800

    Comparing two arrays

--- IAS2.js
+++ IAS2.js
@@ -3,35 +3,81 @@
 //You can return the array with its elements in any order.
 //
 //diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.
-//["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
-//["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with one item.
-//["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
-//["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return an array with two items.
-//["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
-//["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return an empty array.
-//[1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
-//[1, 2, 3, 5], [1, 2, 3, 4, 5] should return an array with one item.
-//[1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
-//[1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return an array with two items.
-//[], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
-//[], ["snuffleupagus", "cookie monster", "elmo"] should return an array with three items.
-//[1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
-//[1, "calf", 3, "piglet"], [7, "filly"] should return an array with six items.
 
-function diffArray(arr1, arr2) {
-  var newArr = [];
-  for(var i=0; i<=arr1.length; i++){
-      if(arr2.indexOf(arr1[i]) !== 0){
-          newArr.push(arr1[i])
-      }
-      if(arr1.indexOf(arr2[i]) !== 0){
-          newArr.push(arr2[i])
-      }
-  }    
-  return newArr;
+//["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+
+
+//["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+
+//["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+
+//[1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+
+//[], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+
+//[1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+
+///////Different approach/////////
+
+
+function diffArray(arr1, arr2){
+    return arr1.concat(arr2).filter(function(e) {return !arr1.includes(e) || !arr2.includes(e)})
 }
 
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////
+
+
+
+
+//function diffArray(arr1, arr2) {
+//    var newArr = [];
+//    for(var i=0; i<=arr1.length-1; i++){
+//        if(arr2.includes(arr1[i])){
+//            continue;
+//        } else { newArr.push(arr1[i])} 
+//    }
+//    for(var i=0; i<=arr2.length-1; i++){
+//      if(arr1.includes(arr2[i])){
+//          continue;
+//      } else { newArr.push(arr2[i])} 
+//  }    
+//  return newArr;
+//}
+
+//[1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
 
+//["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 
+//["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 
+//["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+console.log(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]));
+
+//[1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
+
+//[], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+console.log(diffArray([], ["snuffleupagus", "cookie monster", "elmo"]));
+
+//[1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"]));
+    
+    
+    
+    
+    
+    
+    
+    
