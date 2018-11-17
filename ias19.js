29e344750285f81a2f2b66a2d84cb03c9392da8c
Ian Aranha <ianorama@gmail.com>
2018-11-16 19:44:19 -0800

    Checking truthy of an object

---
+++ ias19.js
@@ -0,0 +1,65 @@
+//Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+//
+//In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+//
+//In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+//
+//Remember, you can access object properties through either dot notation or [] notation.
+
+function truthCheck(collection, pre) 
+{
+    var truth_flag = 0
+    
+    for(var c in collection)
+    {
+        if(collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre]))
+        {
+            truth_flag += 1
+        }
+    }
+    
+    if(truth_flag === collection.length)
+    {
+        return true
+    } 
+    else 
+    {
+        return false
+    }
+}
+
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")) 
+//should return true.
+
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")) 
+//should return false.
+
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age")) 
+//should return false.
+
+
+console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat"))
+//should return false
+
+
+console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat"))
+//should return true
+
+
+console.log(truthCheck([{"single": "yes"}], "single"))
+//should return true
+
+
+console.log(truthCheck([{"single": ""}, {"single": "double"}], "single"))
+//should return false
+
+
+console.log(truthCheck([{"single": "double"}, {"single": undefined}], "single"))
+//should return false
+
+
+console.log(truthCheck([{"single": "double"}, {"single": NaN}], "single"))
+//should return false
